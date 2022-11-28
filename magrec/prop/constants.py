9b1b113e91ed276b376795f1fc13bf65a4c68192
Michael Flaks <misha.flaks@gmail.com>
2022-11-28 23:26:02 +0100

    Add function to convert SI prefixes

--- magrec/prop/constants.py
+++ magrec/prop/constants.py
@@ -19,3 +19,29 @@ MU0: float = 1.25663706212  # [mT * mm / A]
 """Magnetic permeability of free space, 4π * 1e-7 in [H/m = T * m / A]."""
 
 EPSILON0 = 8.854187817e-12
+
+    
+def unit_to_exponent(unit: str):
+    """
+    Convert a unit prefix to an exponent.
+    """
+    if len(unit) == 1:
+        return 1
+    
+    prefix = unit[0]
+    
+    match prefix:
+        case 'n':
+            return 1e-9
+        case 'u':
+            return 1e-6
+        case 'μ':
+            return 1e-6
+        case 'm':
+            return 1e-3
+        case 'k':
+            return 1e3
+        case 'M':
+            return 1e6
+        case 'G':
+            return 1e9
