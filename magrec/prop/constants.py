7d9570146cd5d9b10136da3294e925bc43b634a1
Michael Flaks <misha.flaks@gmail.com>
2022-12-13 09:23:41 +0100

    Reconstruction with optimization and neural net

--- magrec/prop/constants.py
+++ magrec/prop/constants.py
@@ -16,6 +16,8 @@
 twopi = 6.2831853071795862
 
 MU0: float = 1.25663706212  # [mT * mm / A]
+# 
+# uT * um / ?
 """Magnetic permeability of free space, 4π * 1e-7 in [H/m = T * m / A]."""
 
 EPSILON0 = 8.854187817e-12
