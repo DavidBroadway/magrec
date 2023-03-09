16e26374a8327fad2904dcb73ae8f263bfd97682
Michael Flaks <misha.flaks@gmail.com>
2023-03-09 02:13:24 +0100

    Add arch to reconstruct larger field-of-view

--- magrec/nn/arch.py
+++ magrec/nn/arch.py
@@ -10,8 +10,8 @@ __author__ = "Adrien Dubois"
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
 
-import numpy as np
 
 # Variotional estimator is used to calculate uncertainties on the reconstructed parameters. So far we do not do this.
 # @variational_estimator
@@ -374,3 +374,161 @@ class FCCNN(torch.nn.Module):
         x = F.relu(self.conv5(x))
         x = self.conv6(x)
         return x
+
+
+class GioCNN(torch.nn.Module):
+    def __init__(
+        self,
+        kernel=5,
+        stride=2,
+        padding=2,
+        n_channels_in=1,
+        n_channels_out=1,
+        size_out=None,
+    ):
+
+        """
+        Create the net that takes an image of currents of size (3, W, H) and creates another image (3, W, H).
+        3 corresponds to the number of channels in the input image. W and H must be multiples of 2^4 = 16, because the
+        net has 4 convolution layers if stride = 2.
+
+        Args:
+            n_channels_in:  number of channels in input image (number of components)
+            size:           kinda channel inflation parameter, inner convolution layers give size * 8 or size * 16 output parameters
+            kernel:         kernel size
+            stride:         step in which to do the convolution
+            padding:        whether to pad input image for convolution and by how much
+
+        Returns:
+            GeneratorCNN:   the net
+        """
+        super().__init__()
+
+        self.n_channels_in = n_channels_in
+        self.n_channels_out = n_channels_out
+        self.size_out = size_out
+
+        self.relu = nn.LeakyReLU(0.2)
+
+        self.convi = nn.Conv2d(n_channels_in, 8, kernel, 1, padding)
+        self.conv1 = nn.Conv2d(8, 8, kernel, stride, padding)
+        self.conv2 = nn.Conv2d(8, 16, kernel, stride, padding)
+        self.conv3 = nn.Conv2d(16, 32, kernel, stride, padding)
+        self.conv4 = nn.Conv2d(32, 64, kernel, stride, padding)
+
+        self.conv5 = nn.Conv2d(64, 128, 5, 1, 2)
+
+        self.trans1 = nn.ConvTranspose2d(128, 64, kernel, stride, padding, 1)
+        self.trans2 = nn.ConvTranspose2d(64, 32, kernel, stride, padding, 1)
+        self.trans3 = nn.ConvTranspose2d(32, 16, kernel, stride, padding, 1)
+        self.trans4 = nn.ConvTranspose2d(16, 8, kernel, stride, padding, 1)
+        self.conv6 = nn.Conv2d(8, n_channels_out, kernel, 1, padding)
+        self.conv7 = nn.Conv2d(n_channels_out, n_channels_out, kernel, 1, padding)
+
+    def forward(self, x):
+
+        conv0 = self.convi(x)
+        conv0 = self.relu(conv0)
+        conv1 = self.relu(self.conv1(conv0))
+        conv2 = self.relu(self.conv2(conv1))
+        conv3 = self.relu(self.conv3(conv2))
+        conv4 = self.relu(self.conv4(conv3))
+
+        conv5 = self.relu(self.conv5(conv4))
+
+        trans1 = self.relu(self.trans1(conv5))
+        trans2 = self.relu(self.trans2(trans1))
+        trans3 = self.relu(self.trans3(trans2))
+        trans4 = self.relu(self.trans4(trans3))
+
+        conv6 = self.conv6(trans4)
+        out = self.conv7(conv6)
+
+        # force to be of `self.size_out` if it is not None
+        if self.size_out:
+            out = F.interpolate(out, size=self.size_out, mode="bicubic")
+
+        return out
+
+
+# Classes Block, Encoder, Decoder and UNet are taken from https://amaarora.github.io/posts/2020-09-13-unet.html
+# and from communication with Giovanni P.
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3)
+        self.relu = nn.LeakyReLU(0.2)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3)
+
+    def forward(self, x):
+        return self.conv2(self.relu(self.conv1(x)))
+
+
+class Encoder(nn.Module):
+    def __init__(self, chs=(3, 64, 128, 256, 512, 1024)):
+        super().__init__()
+        self.enc_blocks = nn.ModuleList(
+            [Block(chs[i], chs[i + 1]) for i in range(len(chs) - 1)]
+        )
+        self.pool = nn.MaxPool2d(2)
+
+    def forward(self, x):
+        ftrs = []
+        for block in self.enc_blocks:
+            x = block(x)
+            ftrs.append(x)
+            x = self.pool(x)
+        return ftrs
+
+
+class Decoder(nn.Module):
+    def __init__(self, chs=(1024, 512, 256, 128, 64)):
+        super().__init__()
+        self.chs = chs
+        self.upconvs = nn.ModuleList(
+            [nn.ConvTranspose2d(chs[i], chs[i + 1], 2, 2) for i in range(len(chs) - 1)]
+        )
+        self.dec_blocks = nn.ModuleList(
+            [Block(chs[i], chs[i + 1]) for i in range(len(chs) - 1)]
+        )
+
+    def forward(self, x, encoder_features):
+        for i in range(len(self.chs) - 1):
+            x = self.upconvs[i](x)
+            enc_ftrs = self.crop(encoder_features[i], x)
+            x = torch.cat([x, enc_ftrs], dim=1)
+            x = self.dec_blocks[i](x)
+        return x
+
+    def crop(self, enc_ftrs, x):
+        _, _, H, W = x.shape
+        enc_ftrs = torchvision.transforms.CenterCrop([H, W])(enc_ftrs)
+        return enc_ftrs
+
+
+class GioUNet(nn.Module):
+    def __init__(
+        self,
+        n_channels_in=1,
+        n_channels_out=2,
+        enc_chs=(16, 32, 64),
+        dec_chs=(64, 32, 16),
+        size_out=None,
+    ):
+        super().__init__()
+        self.n_channels_in = n_channels_in
+        self.n_channels_out = n_channels_out
+        enc_chs = (n_channels_in,) + enc_chs
+        dec_chs = dec_chs
+        self.encoder = Encoder(chs=enc_chs)
+        self.decoder = Decoder(chs=dec_chs)
+        self.head = nn.Conv2d(dec_chs[-1], n_channels_out, 1)
+        self.size_out = size_out
+
+    def forward(self, x):
+        enc_ftrs = self.encoder(x)
+        out = self.decoder(enc_ftrs[::-1][0], enc_ftrs[::-1][1:])
+        out = self.head(out)
+        if self.size_out:
+            out = F.interpolate(out, self.size_out, mode="bicubic")
+        return out
