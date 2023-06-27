9c026f3636107339538d38a173fee38752113bef
Michael Flaks <misha.flaks@gmail.com>
2023-06-28 01:04:30 +0200

    Add vector field visualization of current and magnetic fields
    Uses plotly to visualize two vector fields of the current distribution
    and the magnetic field that the current distribution
    produces.

--- magrec/misc/plotly_plot.py
+++ magrec/misc/plotly_plot.py
@@ -64,3 +64,70 @@ def plot_vector_field(xyz, uvw, show=False, **kwargs):
         fig.show()
 
     return fig
+
+
+def visualize_vector_fields(magnetic_field, current_distribution, z1, z0):
+    """
+    Visualizes the magnetic field and current distribution in 3D.
+
+    Parameters:
+    - magnetic_field: tensor of shape (3, W, H) representing magnetic field vectors
+    - current_distribution: tensor of shape (2, W, H) representing current distribution vectors
+    - z1: z-coordinate for the magnetic field
+    - z0: z-coordinate for the current distribution
+    """
+
+    # Grid points in x and y direction
+    W, H = magnetic_field.shape[1], magnetic_field.shape[2]
+    x, y = np.meshgrid(np.arange(W), np.arange(H), indexing='ij')
+
+    # Magnetic field vectors
+    x_m, y_m, z_m = x.flatten(), y.flatten(), np.full(W*H, z1)
+    u_m, v_m, w_m = magnetic_field[0].flatten(), magnetic_field[1].flatten(), magnetic_field[2].flatten()
+
+    # Norm of the magnetic field vectors
+    norm_m = np.sqrt(u_m**2 + v_m**2 + w_m**2)
+    
+    # Hover text for magnetic field
+    hover_text_m = ['Norm: {:.4f}<br>Coordinates: ({}, {}, {})<br>Components: ({:.4f}, {:.4f}, {:.4f})'.format(
+        norm, x, y, z1, u, v, w) for norm, x, y, u, v, w in zip(norm_m, x_m, y_m, u_m, v_m, w_m)]
+
+    # Current distribution vectors
+    x_c, y_c, z_c = x.flatten(), y.flatten(), np.full(W*H, z0)
+    u_c, v_c = current_distribution[0].flatten(), current_distribution[1].flatten()
+
+    # Norm of the current distribution vectors
+    norm_c = np.sqrt(u_c**2 + v_c**2)
+    
+    # Hover text for current distribution
+    hover_text_c = ['Norm: {:.4f}<br>Coordinates: ({}, {}, {})<br>Components: ({:.4f}, {:.4f})'.format(
+        norm, x, y, z0, u, v) for norm, x, y, u, v in zip(norm_c, x_c, y_c, u_c, v_c)]
+
+    # Create figure
+    fig = go.Figure()
+
+    # Add magnetic field vectors as cones
+    fig.add_trace(go.Cone(x=x_m, y=y_m, z=z_m,
+                          u=u_m, v=v_m, w=w_m,
+                          sizeref=1, anchor="tail", showscale=False,
+                          name="Magnetic Field",
+                          text=hover_text_m, hoverinfo='text'))
+
+    # Add current distribution vectors as cones
+    fig.add_trace(go.Cone(x=x_c, y=y_c, z=z_c,
+                          u=u_c, v=v_c, w=np.zeros(W*H),
+                          sizeref=1, anchor="tail", showscale=False,
+                          name="Current Distribution",
+                          text=hover_text_c, hoverinfo='text'))
+
+    # Set the 3D scene configurations
+    fig.update_scenes(aspectmode="cube")
+
+    zrange = z1 - z0
+
+    # Set layout and title
+    fig.update_layout(title="Vector Fields Visualization in 3D",
+                      scene=dict(zaxis=dict(range=[z0 - zrange * 0.1, z1 + zrange * 0.1])))
+
+    # Show figure
+    fig.show()
