4ce439b24f75cc9f0e179fe6f0088c87eb09a770
Michael Flaks <misha.flaks@gmail.com>
2023-06-28 11:48:21 +0200

    Fix data path definition
    Data is just shorter than database

--- magrec/__init__.py
+++ magrec/__init__.py
@@ -1,6 +1,6 @@
 import pathlib
 
-__datapath__ = pathlib.Path(__file__).parent.resolve() / "database"
+__datapath__ = pathlib.Path(__file__).parent.resolve() / "data"
 """Standard path to the data directory."""
 
 __logpath__ = pathlib.Path(__file__).parent.parent.resolve() / "logs"
