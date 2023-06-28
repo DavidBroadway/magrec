246bdf88fbe39e8d4c5af6c2529157b8f88c719c
Michael Flaks <misha.flaks@gmail.com>
2023-06-28 11:47:22 +0200

    Fix missing default log path

--- magrec/__init__.py
+++ magrec/__init__.py
@@ -2,3 +2,6 @@ import pathlib
 
 __datapath__ = pathlib.Path(__file__).parent.resolve() / "database"
 """Standard path to the data directory."""
+
+__logpath__ = pathlib.Path(__file__).parent.parent.resolve() / "logs"
+"""Standard path to the log directory."""
